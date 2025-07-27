38a4524a9cd8abc48b5d3c6f3db923bd6ce70e5c
Alfakynz <alfakynz.nyxaeris@gmail.com>
2025-07-27 20:35:32 +0200

    Update CLI tools
    - Add function to update modpack version
    - Add export for all launcher
    - Quit now return on main.py

--- CLI/Utils/get_launcher.py
+++ CLI/Utils/get_launcher.py
@@ -1,5 +1,8 @@
 from Utils.menu import menu
 
-def get_launcher():
-    launcher = menu(["Modrinth", "CurseForge"], "Select a launcher:")
+def get_launcher(all = False):
+    if all:
+        launcher = menu(["All", "Modrinth", "CurseForge"], "Select a launcher:")
+    else:
+        launcher = menu(["Modrinth", "CurseForge"], "Select a launcher:")
     return launcher
--- CLI/main.py
+++ CLI/main.py
@@ -1,14 +1,22 @@
 from Utils.menu import menu
 from build_pack_content import build_pack_content
 from export_modpack import export_modpack
+from update_modpack_version import update_modpack_version
 
 def main():
-    select = menu(["Export modpack", "Build pack content"], "What do you want to do?")
+    select = menu(["Export modpack", "Build pack content", "Update modpack version"], "What do you want to do?")
     match select:
         case "Export modpack":
             export_modpack()
+            main()
         case "Build pack content":
             build_pack_content()
+            main()
+        case "Update modpack version":
+            update_modpack_version()
+            main()
+        case None:
+            return
         case _:
             print("Invalid selection")
 
