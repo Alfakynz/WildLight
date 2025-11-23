85a4a091b322ef1706246e05bc52b63f7cb1436f
Alfakynz <alfakynz.nyxaeris@gmail.com>
2025-11-23 14:49:37 +0100

    Add particles to the Deeper & Darker portal

--- src/client/java/com/alfakynz/wildlight/mixin/DeeperDarkerCompatMixin.java
+++ src/client/java/com/alfakynz/wildlight/mixin/DeeperDarkerCompatMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -25,5 +26,18 @@ public abstract class DeeperDarkerCompatMixin extends CustomPortalBlock {
     @Environment(EnvType.CLIENT)
     private void afterAnimateTick(BlockState state, Level world, BlockPos pos, RandomSource random, CallbackInfo ci) {
         ci.cancel();
+
+        for (int i = 0; i < 1; i++) {
+            double x = pos.getX() + 0.5 + (random.nextDouble() - 0.5) * 0.8;
+            double y = pos.getY() + random.nextDouble() * 0.5;
+            double z = pos.getZ() + 0.5 + (random.nextDouble() - 0.5) * 0.8;
+            world.addParticle(
+                new net.minecraft.core.particles.DustParticleOptions(
+                    new Vec3(0.0, 0.95, 0.85).toVector3f(), 0.75f
+                ),
+                x, y, z,
+                0.0, 0.0, 0.0
+            );
+        }
     }
 }
